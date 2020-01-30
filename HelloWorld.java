53d22d24d361272f28eea769b23b1983fb1c50d9
hjhheha <994776616@qq.com>
2020-01-30 12:30:20 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,24 @@
-public class HelloWorld{
-    public static void main(String[] args){
-        System.out.println("Hello World");
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Objects;
+
+import javax.lang.model.type.NullType;
+
+public class HelloWorld {
+    public static void main(final String[] args){
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.add(1);
+        ll.add(1);
+        ll.add(2);
+        for(Integer i : ll){
+            System.out.println(i);
+        }
+
+
+        
+        
     }
 }
+
